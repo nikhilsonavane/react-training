766c9a2e6e3a6219507064bf86f47e254081df0a
Nikhil Sonavane <nsonavane@integconsulting.com>
2016-02-09 16:05:22 +0530

    set up prog

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nikhil on 09/02/2016.
+ */
+import ReactDOM from "react-dom"
+import React from "react"
+
+ReactDOM.render(
+    <div>
+        hyyyy
+    </div>
+
+    , document.getElementById("app")
+);
