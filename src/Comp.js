6a0654015b8e3bad20b4edf91b2ce376ec5fa3c0
Nikhil Sonavane <nsonavane@integconsulting.com>
2016-02-22 18:16:30 +0530

    calculator using redux

--- src/Comp.js
+++ src/Comp.js
@@ -5,16 +5,27 @@ import React from "react";
 import { connect } from "react-redux"
 
 export const ADD_TEXT = "ADD_TEXT";
-export const MULTIPLICATION="MULTIPLICATION";
 
-const Comp = ({text,multiplication}) => {
-  let num1,num2;
+export const MATH_ACTION = {
+    MULTIPLICATION : 'MULTIPLICATION',
+    ADDITION: 'ADDITION',
+    SUBTRACTION:'SUBTRACTION',
+    DIVIDE:'DIVIDE' };
+
+const Comp = ({text,multiplication,addition,subtraction,divide}) => {
+  let num1, num2;
   return (
       <div>
-        <input type="text" ref={(node)=>{num1=node}} width="30"   /> *
-        <input type="text" ref={(node)=>{num2=node}} width="30"  />
-        <input type="button" onClick={()=> { multiplication(num1.value,num2.value); }} value="MULTIPLICATION"/><br/>
+        Number 1: <input type="text" ref={(node)=>{num1=node}} width="30"/><br/>
+        Number 2: <input type="text" ref={(node)=>{num2=node}} width="30"/><br/>
+
+        <input type="button" onClick={()=> { addition(num1.value,num2.value); }} value="+"/>
+        <input type="button" onClick={()=> { subtraction(num1.value,num2.value); }} value="-"/>
+        <input type="button" onClick={()=> { multiplication(num1.value,num2.value); }} value="*"/>
+        <input type="button" onClick={()=> { divide(num1.value,num2.value); }} value="/"/><br/>
+
         <input type="text" value={text}/>
+
       </div>
   );
 }
@@ -27,8 +38,17 @@ var mapStattoProps = (state) => {
 //dispacth create Action
 var mapDispatchtoProp = (dispatch)=> {
   return {
-    multiplication: (num1,num2)=> {
-      dispatch({ type: MULTIPLICATION , num1,num2})
+    multiplication: (num1, num2)=> {
+      dispatch({type: MATH_ACTION.MULTIPLICATION, num1, num2})
+    },
+    addition: (num1,num2) =>{
+      dispatch({type: MATH_ACTION.ADDITION, num1, num2})
+    },
+    subtraction: (num1,num2) =>{
+      dispatch({type: MATH_ACTION.SUBTRACTION, num1, num2})
+    },
+    divide: (num1,num2) =>{
+      dispatch({type: MATH_ACTION.DIVIDE, num1, num2})
     }
   };
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,15 +3,20 @@
  */
 
 import React from "react"
-import { ADD_TEXT , MULTIPLICATION } from "./Comp"
+import { ADD_TEXT , MATH_ACTION } from "./Comp"
 
 export default  (state='', action='') => {
   switch (action.type) {
     case ADD_TEXT:
         return action.text;
-    case MULTIPLICATION:
+    case  MATH_ACTION.MULTIPLICATION:
         return (action.num1 * action.num2 );
-
+    case  MATH_ACTION.ADDITION:
+      return (Number(action.num1) + Number(action.num2) );
+    case  MATH_ACTION.SUBTRACTION:
+      return (action.num1 - action.num2 );
+    case  MATH_ACTION.DIVIDE:
+      return (action.num1 / action.num2 );
     default :
       return state;
   }
